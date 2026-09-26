c5922694a06d896df6ebd97a8fd14c43a8db2707
sanjay-posguru <s.krishnan@deliveryguru.com>
2026-09-26 14:37:57 +0530

    recomendations

--- services/resume_skill_extractor.py
+++ services/resume_skill_extractor.py
@@ -10,13 +10,13 @@ def extract_skills(text, available_skills):
     detected_skills = []
 
     for skill in available_skills:
-        skill_name = skill["skill_name"].lower()
+        skill_name = skill["skill_name"]
 
         # Escape special regex characters
         pattern = re.escape(skill_name)
 
         # Match the skill anywhere in the resume
         if re.search(pattern, text, re.IGNORECASE):
-            detected_skills.append(skill_name)
+            detected_skills.append(skill)
 
     return detected_skills
